5e261133f6b9652ec654986d2ffd9b8d1f0a8470
Stanislav Kvashchuk <StanislavK.1982@gmail.com>
2016-07-07 00:49:42 +0300

    Commit1

---
+++ src/main/java/com/java/courses/servlets/HelloWorldServlet.java
@@ -0,0 +1,64 @@
+package com.java.courses.servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.sql.*;
+import java.util.Properties;
+
+public class HelloWorldServlet extends HttpServlet {
+    private Connection connection;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html>");
+        writer.println("<head>");
+        writer.println("</head>");
+        writer.println("<body>");
+        writer.println("<p>Hello World1<p/>");
+
+        try {
+            getStudentsFromJDBC(writer, connection);
+        } catch (SQLException e) {
+            writer.println("<p>"+e.getMessage()+"</p>");
+
+        }
+/*
+        String str="";
+        try {
+            str=getStudentsFromJDBC();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        writer.print(str);
+        */
+        writer.println("<p>Hello World2<p/>");
+
+        writer.println("</body>");
+        writer.println("</html>");
+    }
+
+
+    public void getStudentsFromJDBC(PrintWriter writer, Connection connection) throws SQLException, IOException {
+        Properties properties = new Properties();
+        InputStream stream = getClass().getResourceAsStream("db.properties");
+        properties.load(stream);
+        connection = DriverManager.getConnection(properties.getProperty("url"),
+                properties.getProperty("username"),
+                properties.getProperty("password"));
+        writer.println("Test1");
+        String sql = "select * from students";
+        Statement statement = connection.createStatement();
+        statement.execute(sql);
+        ResultSet resultSet = statement.getResultSet();
+        //String str = "";
+        while (resultSet.next()) {
+            writer.println("<p>" + resultSet.getString("name") + "</p>");
+            writer.println("<p>" + resultSet.getString("surename") + "</p>");
+        }
+    }
+}
